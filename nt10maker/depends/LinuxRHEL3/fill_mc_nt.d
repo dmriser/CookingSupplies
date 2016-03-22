2dff3059e8bc39383c108d563b1b7fc4e55bf657
dmriser <dmriser@gmail.com>
2016-03-22 08:22:42 -0400

    global .gitognore fix

---
+++ nt10maker/depends/LinuxRHEL3/fill_mc_nt.d
@@ -0,0 +1,1 @@
+/home/gohn/top_dir/lib/LinuxRHEL3/libnt10maker.a(fill_mc_nt.o) ./depends/LinuxRHEL3/fill_mc_nt.d:  fill_mc_nt.F bcs.inc mc_nt.inc
---
+++ nt10maker/depends/LinuxRHEL3/filter.d
@@ -0,0 +1,1 @@
+/home/gohn/top_dir/lib/LinuxRHEL3/libnt10maker.a(filter.o) ./depends/LinuxRHEL3/filter.d:  filter.F
